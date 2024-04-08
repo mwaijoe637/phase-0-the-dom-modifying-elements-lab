dfe58bae6f29673bdbf504dfee06087c289d7b8a
joe <brakejoe9@gmail.com>
2024-04-08 08:07:18 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'Joe is the champion'; 
+
+
+document.body.appendChild(newHeader);
